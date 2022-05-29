51cbbd41ef5af888781af277cb7cc38ace1285b3
Tatiane Rodrigues <93277324+tatianerodriguess@users.noreply.github.com>
2022-05-29 15:41:54 -0300

    Create Calculadora.java
    Classe Calculadora iniciada

---
+++ Relatorio ads/RelatorioFinal/src/Calculadora.java
@@ -0,0 +1,23 @@
+import java.lang.Math;
+
+public class Calculadora {
+
+    /* Por favor declarar aqui as funções restantes, para cada integrante duas funções como pedido */
+
+
+
+
+    /* declaração abaixo das minhas duas funções: exponencial e raíz quadrada -that */
+
+    public static int exponencial (int base, int expoente) {
+        return (int) Math.pow(base, expoente);
+    }
+
+    public static double raizquadrada (double numero) {
+        return (double) Math.sqrt(numero);
+    }
+
+
+    /* Falta implementar: soma, subtração, multiplicação, divisão, piso e teto */
+
+}
