9ddd40e3d6fc158365f3212111fde47a756124e5
Tatiane Rodrigues <93277324+tatianerodriguess@users.noreply.github.com>
2022-06-04 01:37:35 -0300

    ClasseIMC
    Criação da classe IMC utilizando métodos da classe Calculadora

--- Relatorio ads/RelatorioFinal/src/Calculadora.java
+++ Relatorio ads/RelatorioFinal/src/Calculadora.java
@@ -3,43 +3,36 @@ import java.lang.Math;
 public class Calculadora {
 
 
- /* declaração abaixo das funções: soma, subtração e multiplicação - Juliane */
-
-    public static double soma (double numero1, double numero2) {
+    public double soma (double numero1, double numero2) {
        return numero1 + numero2;
     }
 
-    public static double subtracao (double numero1, double numero2) {
+    public double subtracao (double numero1, double numero2) {
        return numero1 - numero2;
     }
 
-    public static double multiplicação (double numero1, double numero2) {
+    public double multiplicação (double numero1, double numero2) {
        return numero1 * numero2;
     }
 
-    public static double divisão (double numero1, double numero2) {
+    public double divisão (double numero1, double numero2) {
        return numero1 / numero2;
     }
 
-    /* declaração abaixo das minhas duas funções: ex,ponencial e raíz quadrada -that */
-
-    public static int exponencial (int base, int expoente) {
+    public int exponencial (int base, int expoente) {
         return (int) Math.pow(base, expoente);
     }
 
-    public static double raizquadrada (double numero) {
+    public double raizquadrada (double numero) {
         return (double) Math.sqrt(numero);
     }
 
-    public static double piso (double numero) {
+    public double piso (double numero) {
        return (double) Math.floor(numero);
     }
 
-    public static double teto (double numero) {
+    public double teto (double numero) {
        return (double) Math.ceil(numero);
     }
-
-
-    /* Falta implementar: divisão, piso e teto */
 
 }
--- Relatorio ads/RelatorioFinal/src/CalculoIMC.java
+++ Relatorio ads/RelatorioFinal/src/CalculoIMC.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
+
 public class CalculoIMC {
+       
+    public static void main(String[] args) {
+
+        Calculadora calculadoraimc = new Calculadora();
+        
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("_____________________________________________");
+        System.out.println("|    Digite seu peso:       /Ex.: 65,5       |");  
+        double peso = input.nextDouble();
+        System.out.println("|    Digite sua altura:     /Ex.: 1,68       |");
+        double altura = input.nextDouble();
     
-    /* that: implementação da classe 1 com uso da calculadora, código ainda a ser feito... */
-
-
+        double imc = calculadoraimc.divisão(peso , calculadoraimc.multiplicação(altura, altura));
+    
+            System.out.println("|Seu IMC é: " + imc + "               |");
+        System.out.println("_____________________________________________");
+        }
+        
 }
