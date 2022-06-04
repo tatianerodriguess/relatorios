269539ff7fa7577108015a42cb57af096a03813d
GLEISON SILVA DE LIMA <106377782+Gleison1978@users.noreply.github.com>
2022-06-04 14:25:47 -0300

    Create fatorial.java

---
+++ Relatorio ads/RelatorioFinal/src/fatorial.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class fatorial {
+    public static void main(String[] args) {
+
+        Calculadora calcfatorial = new Calculadora();
+        
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Digite o número que deseja encontrar o fatorial:");
+        double valor = input.nextDouble();
+        double x = valor;
+
+        if (valor == 0) {
+            System.out.println("O fatorial de 0 é 1.");
+        } else {
+            double fat = valor;
+            while (valor > 1) {
+                fat = calcfatorial.multiplicação(fat, (valor-1));
+                valor--;
+            }
+            System.out.println("O fatorial de " + x + " é: " + fat);
+        }
+    }
+}
